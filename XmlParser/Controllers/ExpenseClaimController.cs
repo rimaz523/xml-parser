47d6115be464ee89dc47679366299eb942a036de
Mohommed Rimaz <rimazmohommed523@gmail.com>
2020-12-05 15:28:52 +1300

    initial commit

---
+++ XmlParser/Controllers/ExpenseClaimController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace XmlParser.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ExpenseClaimController : ControllerBase
+    {
+        private readonly ILogger<ExpenseClaimController> _logger;
+
+        public ExpenseClaimController(ILogger<ExpenseClaimController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public ActionResult<string> Post()
+        {
+            return "workss!";
+        }
+    }
+}
