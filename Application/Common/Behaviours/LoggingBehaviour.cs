1feb79d3449e621ef65478689fb8498f740d304d
Mohommed Rimaz <rimazmohommed523@gmail.com>
2020-12-05 19:18:02 +1300

    add logging for requests

---
+++ Application/Common/Behaviours/LoggingBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using MediatR.Pipeline;
+using Microsoft.Extensions.Logging;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Common.Behaviours
+{
+    public class LoggingBehaviour<TRequest> : IRequestPreProcessor<TRequest>
+    {
+        private readonly ILogger _logger;
+
+        public LoggingBehaviour(ILogger<TRequest> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task Process(TRequest request, CancellationToken cancellationToken)
+        {
+            var requestName = typeof(TRequest).Name;
+            _logger.LogInformation("XmlParser Request: {Name} {@Request}",
+                requestName, request);
+        }
+    }
+}
--- XmlParser/Controllers/ExpenseClaimController.cs
+++ XmlParser/Controllers/ExpenseClaimController.cs
@@ -1,6 +1,5 @@
 ﻿using Application.ExpenseClaims.Commands.CreateExpenseClaim;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using System.Threading.Tasks;
 
 namespace XmlParser.Controllers
@@ -9,13 +8,6 @@ namespace XmlParser.Controllers
     [Route("api/[controller]")]
     public class ExpenseClaimController : ApiControllerBase
     {
-        private readonly ILogger<ExpenseClaimController> _logger;
-
-        public ExpenseClaimController(ILogger<ExpenseClaimController> logger)
-        {
-            _logger = logger;
-        }
-
         [HttpPost]
         public async Task<ActionResult<ExpenseClaimResponseDto>> Post(CreateExpenseClaimCommand command)
         {
