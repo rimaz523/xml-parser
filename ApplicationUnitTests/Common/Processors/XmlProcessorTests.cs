7b1c901602ed8bbbd09d3e6e3f5f88649858baa0
Mohommed Rimaz <rimazmohommed523@gmail.com>
2020-12-06 16:48:10 +1300

    add unit tests

--- ApplicationUnitTests/Common/Processors/XmlProcessorTests.cs
+++ ApplicationUnitTests/Common/Processors/XmlProcessorTests.cs
@@ -21,5 +21,46 @@ namespace ApplicationUnitTests.Common.Processors
 
             Assert.IsTrue(result);
         }
+
+        [Test]
+        public void IsTagPresent_WhenStartTagMissingInXml_ReturnFalse()
+        {
+            var result = _xmlProcessor.IsTagPresent("test", "this is a </test> xml");
+
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void IsTagPresent_WhenEndTagMissingInXml_ReturnFalse()
+        {
+            var result = _xmlProcessor.IsTagPresent("test", "this is a <test> xml");
+
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void IsTagPresent_WhenTagsMissingInXml_ReturnFalse()
+        {
+            var result = _xmlProcessor.IsTagPresent("test", "this is a <other>12</other> xml");
+
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void GetTagContent_WhenTagAndContentExists_ReturnContent()
+        {
+            var tagContent = "sampleContent";
+            var result = _xmlProcessor.GetTagContent("test", $"this is a <test>{tagContent}</test> xml");
+
+            Assert.AreEqual(result, tagContent);
+        }
+
+        [Test]
+        public void GetTagContent_WhenTagAndContentDoNotExist_ReturnNull()
+        {
+            var result = _xmlProcessor.GetTagContent("test", $"this is a <other>other content</other> xml");
+
+            Assert.IsNull(result);
+        }
     }
 }
