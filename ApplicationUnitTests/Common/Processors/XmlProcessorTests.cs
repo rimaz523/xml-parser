abbccdc727b2a9207d7ba885806f7740fe5c467a
Mohommed Rimaz <rimazmohommed523@gmail.com>
2020-12-06 16:24:09 +1300

    add integration test scenarios + setup NUnit tests

---
+++ ApplicationUnitTests/Common/Processors/XmlProcessorTests.cs
@@ -0,0 +1,25 @@
+﻿using Application.Common.Processors;
+using Application.Common.Processors.Interfaces;
+using NUnit.Framework;
+
+namespace ApplicationUnitTests.Common.Processors
+{
+    public class XmlProcessorTests
+    {
+        private IXmlProcessor _xmlProcessor;
+
+        [SetUp]
+        public void Setup ()
+        {
+            _xmlProcessor = new XmlProcessor();
+        }
+
+        [Test]
+        public void IsTagPresent_WhenTagStartAndEndExistsInXml_ReturnTrue()
+        {
+            var result = _xmlProcessor.IsTagPresent("test", "this is a <test></test> xml");
+
+            Assert.IsTrue(result);
+        }
+    }
+}
